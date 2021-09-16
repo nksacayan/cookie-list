60c50940b3d0b803c9be9a159fdd3584e98c0e66
Nicholas Sacayan (Atom) <nksacayan@gmail.com>
2021-09-16 15:26:40 -0700

    Adds react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Test Button</Button>
       </header>
     </div>
   );
