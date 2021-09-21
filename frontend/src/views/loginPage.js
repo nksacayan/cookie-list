617ffe97db4c598cb6bb0ccfb85908c8365ef849
Nicholas Sacayan (Atom) <nksacayan@gmail.com>
2021-09-21 14:05:59 -0700

    Creates login page

--- frontend/src/views/loginPage.js
+++ frontend/src/views/loginPage.js
@@ -1,18 +1,28 @@
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import Nav from "react-bootstrap/Nav";
 
 function LoginPage() {
   return (
     <>
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Group className="mb-3 mt-3">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" />
         </Form.Group>
+        <Button variant="primary">Submit</Button>
       </Form>
+
+      <Nav>
+        <Nav.Item>
+          <Nav.Link href="/register">Make an account!</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </>
   );
 }
