6b775adba3f5772b7f413cc4428fe7870357be9f
Nicholas Sacayan (Atom) <nksacayan@gmail.com>
2021-09-21 13:59:22 -0700

    Creates register page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,7 +9,7 @@ function MyNavbar() {
       expand="lg"
       className="justify-content-center"
     >
-      <Navbar.Brand href="/">The Chip List</Navbar.Brand>
+      <Navbar.Brand href="/">The Cookie List</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Nav.Link href="/login">Login</Nav.Link>
       <Nav.Link href="/about">About</Nav.Link>
--- frontend/src/views/RegisterPage.js
+++ frontend/src/views/RegisterPage.js
@@ -1,21 +1,23 @@
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function RegisterPage() {
   return (
     <>
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Group className="mb-3 mt-3">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="mb-3">
           <Form.Label>Username</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control type="text" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control type="password" />
         </Form.Group>
+        <Button variant="primary">Submit</Button>
       </Form>
     </>
   );
