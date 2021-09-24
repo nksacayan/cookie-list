aa0129a17863466b8d5cb06b164022ef322a9a67
Nicholas Sacayan (Atom) <nksacayan@gmail.com>
2021-09-23 18:06:53 -0700

    Practices basic express

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,4 +3,30 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+let data = {
+  name: "Nick",
+  age: "18",
+};
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.post("/", (req, res) =>
+  res.send(`a post request with / route on port ${PORT}`)
+);
+
+app.post("/:id", (req, res) => {
+  res.send(`a post request with / route on port ${PORT}`);
+  console.log(req.params.id);
+});
+
+app.put("/", (req, res) =>
+  res.send(`a put request with / route on port ${PORT}`)
+);
+
+app.delete("/", (req, res) =>
+  res.send(`a post delete with / route on port ${PORT}`)
+);
+
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
